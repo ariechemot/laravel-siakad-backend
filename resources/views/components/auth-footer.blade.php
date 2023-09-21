434937cfd422925499992fc15ba251caed428ea9
Ari Putra <ariputra.unpam@gmail.com>
2023-09-21 14:05:56 +0700

    pages auth and home ready

--- resources/views/components/auth-footer.blade.php
+++ resources/views/components/auth-footer.blade.php
@@ -1,3 +1,3 @@
   <div class="simple-footer">
-      Copyright &copy; Stisla 2018
+      Copyright &copy; FIC 8 Ari Putra with Stisla 2018
   </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/blank-page',['type_menu' => '']);
+    return view('pages.auth.auth-login',['type_menu' => '']);
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.app.dashboard-siakad',['type_menu' => '']);
+})->name('dashboard');
+
+
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register',['type_menu' => '']);
+})->name('register');
+
+Route::get('/forgot', function () {
+    return view('pages.auth.auth-forgot-password',['type_menu' => '']);
+})->name('forgot');
+
+Route::get('/reset', function () {
+    return view('pages.auth.auth-reset-password',['type_menu' => '']);
+})->name('reset');
+
+
