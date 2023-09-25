f79694597885c88829b9defe5f80555cfb182f4c
Ari Putra <ariputra.unpam@gmail.com>
2023-09-25 14:23:47 +0700

    FIC Week 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class
+        ]);
+
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function(){
+        return view ('pages.app.dashboard-siakad',['type_menu' => '']);
+    })->name('home');
+
+
+});
+
+ Route::get('/forgot-password', function(){
+        return view ('pages.auth.auth-forgot-password',['type_menu' => '']);
+    })->name('forgot-password');
+
+
+/* Route::get('/', function () {
     return view('pages.auth.auth-login',['type_menu' => '']);
 });
 
@@ -34,5 +50,7 @@ Route::get('/forgot', function () {
 Route::get('/reset', function () {
     return view('pages.auth.auth-reset-password',['type_menu' => '']);
 })->name('reset');
+ */
+
 
 
